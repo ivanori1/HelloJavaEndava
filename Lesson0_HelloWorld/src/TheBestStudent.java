77e4f1a509fbc0081bd9a526b984c325042f7ae8
ivanori1 <coric.ivan@yandex.com>
2018-12-10 11:32:42 +0100

    comment for task add

---
+++ Lesson0_HelloWorld/src/TheBestStudent.java
@@ -0,0 +1,9 @@
+public class TheBestStudent {
+    public static void main(String[] args) {
+        /*
+        Korišćenjem for petlje pronaći i ispisati ime studenta sa najvećim brojem poena na testu.
+        Prvo se unosi broj studenata n, posle se n puta obilazi for petlja i unose ime i broj poena studenta.
+        Zapamtiti onog sa najvećim brojem poena.
+         */
+    }
+}
