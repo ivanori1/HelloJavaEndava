08701c567f0f5de20097081ef0b6a1f32011cd54
ivanori1 <coric.ivan@yandex.com>
2018-12-10 15:14:11 +0100

    lesson2 task 2 finish

--- Lesson0_HelloWorld/src/TheBestStudent.java
+++ Lesson0_HelloWorld/src/TheBestStudent.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TheBestStudent {
     public static void main(String[] args) {
         /*
@@ -5,5 +7,27 @@ public class TheBestStudent {
         Prvo se unosi broj studenata n, posle se n puta obilazi for petlja i unose ime i broj poena studenta.
         Zapamtiti onog sa najvećim brojem poena.
          */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Unesite broj studenata");
+        int brojStudenta = input.nextInt();
+        int najvecaOcjena = 0;
+        String najboljiStudent ="";
+
+        for (int x =0; x < brojStudenta; x ++ ) {
+            int ostatak = brojStudenta - x;
+            int redniBroj = x+1;
+            System.out.println("Ostalo za unos jos: "+ ostatak);
+            System.out.println("Unesi ime studenta rednog broja " + redniBroj);
+            String imeStudenta = input.next();
+            System.out.println("Unesi broj poena studenta od 0 do 100");
+            int ocjenaStudenta = input.nextInt();
+            if (ocjenaStudenta > najvecaOcjena) {
+                najvecaOcjena = ocjenaStudenta;
+                najboljiStudent = imeStudenta;
+            }
+        }
+        System.out.println("Najvecu ocjenu ima " + najboljiStudent + " sa ocjenom "+ najvecaOcjena);
+
     }
 }
