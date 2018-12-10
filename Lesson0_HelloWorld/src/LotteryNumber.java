8c5ed83a6ccf50ee2528bb50306c4b39403ea686
ivanori1 <coric.ivan@yandex.com>
2018-12-10 11:21:37 +0100

    comment for modification add

--- Lesson0_HelloWorld/src/LotteryNumber.java
+++ Lesson0_HelloWorld/src/LotteryNumber.java
@@ -4,6 +4,13 @@ import java.util.Random;
 
 public class LotteryNumber {
     public static void main(String args[]) {
+        /**
+         * Uraditi modifikaciju domaćeg iz prethodne nedelje, zadatak pogađanja broja.
+         * Vrteti se u petlji dok ne pogodimo broj, ili budemo barem blizu (razlika manja od 5).
+         * Taj deo sa proverom pogotka izdvojiti u zasebnu metodu, kojoj se kao argument prosledjuje broj koji tipujemo,
+         * a povratna vrednost metode je 0 – ako smo pogodili, 1 – ako smo bili blizu, 2 – ako smo promašili.
+         * Na osnovu toga ostajemo ili izlazimo iz petlje i završavamo program sa odgovarajućom porukom.
+         */
         Scanner lotteryNumber = new Scanner(System.in);
         System.out.println("Unesite broj od 1 do 50");
         int n = lotteryNumber.nextInt();
