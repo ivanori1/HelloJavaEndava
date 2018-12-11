142b094a964b531fa43975c12cbeda58803a7de8
ivanori1 <coric.ivan@yandex.com>
2018-12-11 16:56:15 +0100

    lesson2 task 3 pull changes make

--- Lesson0_HelloWorld/src/LotteryNumber.java
+++ Lesson0_HelloWorld/src/LotteryNumber.java
@@ -11,13 +11,6 @@ public class LotteryNumber {
      * Na osnovu toga ostajemo ili izlazimo iz petlje i završavamo program sa odgovarajućom porukom.
      */
 
-//    static Scanner input = new Scanner(System.in);
-//    static int zeljeniBroj = input.nextInt();
-//
-//    static Random random = new Random();
-//    static int zamisljeniBroj = random.nextInt(50) + 1;
-//
-//    static int razlika = zamisljeniBroj - zeljeniBroj;
     public static int vratiRezultat(int zeljeniBroj, int zamisljeniBroj) {
         int razlika = zamisljeniBroj - zeljeniBroj;
         if (razlika == 0) {
@@ -31,30 +24,36 @@ public class LotteryNumber {
 
         Random random = new Random();
         int zamisljeniBroj = random.nextInt(50) + 1;
-        System.out.println("Broj je zamisljen! Probajte da ga pogodite.");
+        System.out.println("Broj je zamisljen! Probajte da ga pogodite broj izmedju 1 i 50.");
         Scanner input = new Scanner(System.in);
 
         while (true) {
 
-             System.out.println("Unesi broj");
-             int zeljeniBroj = input.nextInt();
-             int rezultat = vratiRezultat(zeljeniBroj, zamisljeniBroj);
-             if (zeljeniBroj < 50) {
+            System.out.println("Unesi broj");
 
-                 if (rezultat == 0) {
-                     System.out.println("Cestitamo, pogodili ste");
-                     break;
-                 } else if (vratiRezultat(zeljeniBroj, zamisljeniBroj) == 1) {
-                     System.out.println("Blizu ste, pokusavajte jos, razlika izmedju brojeva je 5 ili manja");
-                 } else if (rezultat == 2) {
-                     System.out.println("Pokusaj nije blizu zamisljenog broja, nastavite da pokusavate");
-                 }
-             } else {
-                 System.out.println("Uneseni broj je veci od 50");
-             }
+            if (input.hasNextInt()) {
+                int zeljeniBroj = input.nextInt();
+                int rezultat = vratiRezultat(zeljeniBroj, zamisljeniBroj);
+                if (zeljeniBroj < 50) {
+
+                    if (rezultat == 0) {
+                        System.out.println("Cestitamo, pogodili ste");
+                        break;
+                    } else if (vratiRezultat(zeljeniBroj, zamisljeniBroj) == 1) {
+                        System.out.println("Blizu ste, pokusavajte jos, razlika izmedju brojeva je 5 ili manja");
+                    } else if (rezultat == 2) {
+                        System.out.println("Pokusaj nije blizu zamisljenog broja, nastavite da pokusavate");
+                    }
+                } else {
+                    System.out.println("Uneseni broj je veci od 50");
+                }
+
+            } else {
+                System.out.println(" Upozorenje! nevalidan unos, kraj programa");
+                break;
+            }
+
 
         }
-
-
     }
 }
