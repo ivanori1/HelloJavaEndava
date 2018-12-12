8ce938a427355ec03d0c08046649d77580de0492
ivanori1 <coric.ivan@yandex.com>
2018-12-12 16:30:19 +0100

    init commit

---
+++ Lesson0_HelloWorld/src/ArraysTask.java
@@ -0,0 +1,6 @@
+public class ArraysTask {
+    /*
+    Za uneti niz celih brojeva duzine n (koja se takođe unosi), sortirati niz, ispitati da li sadrži broj x
+    i ispisati prvih 5 članova niza na standardni izlaz. Koristiti klasu Arrays.
+     */
+}
