7f8e2891cbe18e7010f2da9504b868fe207ec6b7
ivanori1 <coric.ivan@yandex.com>
2018-12-13 09:22:26 +0100

    commit

--- Lesson0_HelloWorld/src/ArraysTask.java
+++ Lesson0_HelloWorld/src/ArraysTask.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class ArraysTask {
     /*
     Za uneti niz celih brojeva duzine n (koja se takođe unosi), sortirati niz, ispitati da li sadrži broj x
     i ispisati prvih 5 članova niza na standardni izlaz. Koristiti klasu Arrays.
      */
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Unesite broj clanova niza");
+        int duzinaNiza = input.nextInt();
+        System.out.println("Unesite broj koji je clan niza");
+        int clanoviNiza = 0;
+        for (int x = 0; x < duzinaNiza; x++) {
+            clanoviNiza = input.nextInt();
+        }
+    }
 }
